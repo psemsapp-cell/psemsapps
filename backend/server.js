d73cc35a59eb3688a9380de3fc8fbe6de62fb398
psemsapp-cell <psemsapp@gmail.com>
2026-05-05 13:55:22 +0800

    fix: allow Cache-Control header in CORS

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control']
 }));
 
 // routes
