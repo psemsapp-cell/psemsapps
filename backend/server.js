56a6f4b2fab50b4bf3f9d192a415b79daf0cfda6
psemsapp-cell <psemsapp@gmail.com>
2026-05-05 13:41:47 +0800

    fix: configure CORS to allow all methods

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const app = express();
 require('dotenv').config();
 
 // middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // routes
 app.use('/api/sms', smsRoute);
